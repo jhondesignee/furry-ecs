d31e9f53f187e9c5df68b0c2882085f14bd9f6f1
jhondesignee <jhondesignee@gmail.com>
2024-12-17 13:48:04 -0300

    WIP

--- src/system.ts
+++ src/system.ts
@@ -6,6 +6,14 @@ export default class System {
   public readonly destroy: SystemDestroyFunction | undefined
 
   constructor(config?: SystemConfig) {
+    if (config !== undefined && (typeof config !== "object" || Array.isArray(config) || config === null)) {
+      throw new TypeError("'config' parameter must be an object")
+    }
+    for (const value of Object.values(config || {})) {
+      if (!(value instanceof Function)) {
+        throw new TypeError("System callback must be a function")
+      }
+    }
     this.start = config?.start
     this.update = config?.update
     this.destroy = config?.destroy
