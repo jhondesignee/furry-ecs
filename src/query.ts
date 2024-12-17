ff653f5503e9dd199f1fc7e8b7d1047ab91bcb7c
jhondesignee <jhondesignee@gmail.com>
2024-12-17 13:48:04 -0300

    WIP

--- src/query.ts
+++ src/query.ts
@@ -1,7 +1,7 @@
 import { Status, QueryOperation } from "#constants"
+import World from "#world"
+import Component from "#component"
 import type Entity from "#entity"
-import type Component from "#component"
-import type World from "#world"
 import type { QueryConfig } from "#types"
 
 export default class Query {
@@ -18,11 +18,36 @@ export default class Query {
   private world!: World
 
   constructor(config?: QueryConfig) {
+    if (config !== undefined && (typeof config !== "object" || Array.isArray(config) || config === null)) {
+      throw new TypeError("'config' parameter must be an object")
+    }
+    if (config?.include !== undefined && !Array.isArray(config?.include)) {
+      throw new TypeError("'include' option must be an array of components")
+    }
+    for (const value of config?.include || []) {
+      if (!(value instanceof Component)) {
+        throw new TypeError("'include' option must be an array of components")
+      }
+    }
+    if (config?.exclude !== undefined && !Array.isArray(config?.exclude)) {
+      throw new TypeError("'exclude' option must be an array of components")
+    }
+    for (const value of config?.exclude || []) {
+      if (!(value instanceof Component)) {
+        throw new TypeError("'exclude' option must be an array of components")
+      }
+    }
+    if (config?.includeOperation !== undefined && typeof config?.includeOperation !== "number") {
+      throw new TypeError("Include operation option must be a 'QueryOperation' value")
+    }
+    if (config?.excludeOperation !== undefined && typeof config?.excludeOperation !== "number") {
+      throw new TypeError("Exclude operation option must be a 'QueryOperation' value")
+    }
     this.includeComponents = new Set(config?.include || [])
     this.excludeComponents = new Set(config?.exclude || [])
-    this.entities = new Set()
     this.includeOperation = config?.includeOperation ?? QueryOperation.ALL
     this.excludeOperation = config?.excludeOperation ?? QueryOperation.ANY
+    this.entities = new Set()
     this.operationTable = {
       [QueryOperation.ANY]: this.hasAnyComponents.bind(this),
       [QueryOperation.ALL]: this.hasAllComponents.bind(this),
@@ -31,6 +56,15 @@ export default class Query {
   }
 
   public exec(world: World, status?: Status, component?: Component<any>): Array<Entity> {
+    if (!(world instanceof World)) {
+      throw new TypeError("'world' parameter must be a 'World' instance")
+    }
+    if (status !== undefined && !Object.values(Status).includes(status)) {
+      throw new TypeError("'status' parameter must be a 'Status' value")
+    }
+    if (component !== undefined && !(component instanceof Component)) {
+      throw new TypeError("'component' parameter must be a 'Component' instance")
+    }
     this.world = world
     // call this method every time is inefficient
     this.entities = this.filterEntitiesByComponent()
@@ -42,14 +76,14 @@ export default class Query {
 
   private filterEntitiesByComponent(): Set<Entity> {
     const filteredEntities = new Set<Entity>()
-    if (![...this.includeComponents.keys(), ...this.excludeComponents.keys()].some(item => this.world.components.hasData(item)))
+    if (![...this.includeComponents, ...this.excludeComponents].some(item => this.world.components.hasData(item))) {
       return filteredEntities
+    }
     for (const entity of this.world.entities.keys()) {
-      if (this.operationTable[this.includeOperation](entity, this.includeComponents)) {
+      const includes = this.operationTable[this.includeOperation](entity, this.includeComponents)
+      const excludes = this.operationTable[this.excludeOperation](entity, this.excludeComponents)
+      if (includes && !excludes) {
         filteredEntities.add(entity)
-      }
-      if (this.operationTable[this.excludeOperation](entity, this.excludeComponents)) {
-        filteredEntities.delete(entity)
       }
     }
     return filteredEntities
@@ -58,7 +92,7 @@ export default class Query {
   private filterEntitiesByStatus(status: Status, component?: Component<any>): Array<Entity> {
     const filteredEntities = new Array<Entity>()
     for (const entity of this.entities) {
-      if (component?.entities.getDataStatus(entity) === status || this.world?.entities.getDataStatus(entity) === status) {
+      if (component?.entities.getDataStatus(entity) === status || this.world.entities.getDataStatus(entity) === status) {
         filteredEntities.push(entity)
       }
     }
